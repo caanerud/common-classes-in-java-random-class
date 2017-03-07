5cf732f1624d39caa720a626592f74daad98adcf
caanerud <caanerud.ca@gmail.com>
2017-03-07 15:42:49 -0500

    adding code to git

--- src/main/java/com/theironyard/LuckyNumbers.java
+++ src/main/java/com/theironyard/LuckyNumbers.java
@@ -1,12 +1,15 @@
 package com.theironyard;
 
+
+import java.util.Random;
+
 /**
  * For this exercise you will complete the following class that prints out five
  * lucky numbers from -50 to 100 based on the current timestamp. For example:
- *
+ * <p>
  * The timestamp for Midnight, January 1st, 1970 is 0. Therefore the lucky
  * numbers generated would be: -28, 56, 82, 60, 77
- *
+ * <p>
  * The timestamp for 3:54 am on December 9, 1977 is 250505100000. Therefore the
  * lucky numbers generated would be: 96, 86, 1, 72, 60
  */
@@ -17,20 +20,28 @@ public class LuckyNumbers {
      * Random. Don't initialize it here, you'll create constructors to do that.
      */
     // todo: create private property to hold an instance of Random
+    private Random rand;
+
+
 
     /**
      * Create a no-argument constructor that sets `rand` to an instance of
      * Random.
      */
     // todo: create no-argument constructor
+    public LuckyNumbers(){
+        rand = new Random();
 
+    }
     /**
      * Create a constructor that accepts a long argument. Create a new instance
      * of Random and use the provided argument as its seed.
      * @param timestamp
      */
     // todo: create a constructor that accepts a long argument
-
+    public LuckyNumbers(long timestamp){
+        rand = new Random(timestamp);
+    }
     /**
      * Create a method named fiveLuckyNumbers() that returns nothing. When
      * invoked, this method will generate five lucky numbers from -50 to 100,
@@ -44,5 +55,8 @@ public class LuckyNumbers {
      */
     // todo: create fiveLuckyNumbers() method
 
+    public void fiveLuckyNumbers(){
+        System.out.printf("Your five lucky numbers are: %s, %s, %s, %s, %s%n", rand.nextInt(151 )-50, rand.nextInt(151 )-50, rand.nextInt(151 )-50, rand.nextInt(151 )-50,rand.nextInt(151 )-50);
 
+    }
 }
